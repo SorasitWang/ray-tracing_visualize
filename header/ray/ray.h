0591c724f6a399b25dbefac782d971abe60df5cb
sorasitwang <sorasit789@gmail.com>
2023-02-22 21:52:33 +0700

    WIP refract ray

--- header/ray/ray.h
+++ header/ray/ray.h
@@ -8,20 +8,107 @@
 #include <glm/glm/gtc/matrix_transform.hpp>
 #include <glm/glm/gtc/type_ptr.hpp>
 #include <string>
-
+#include "../shader.h"
 class Ray
 {
 public :
 	glm::vec3 origin, dir;
 	float tMax;
-	Ray() {};
+	unsigned int VAO, VBO;
+	Shader* shader;
+	vector<float> vertexData;
+	glm::vec3 color;
 	~Ray() {};
 	Ray(glm::vec3 rayOrigin, glm::vec3 rayDir) {
 		this->origin = rayOrigin;
 		this->dir = glm::normalize(rayDir);
+		this->shader = new Shader("./header/shader/line.vs", "./header/shader/line.fs", "./header/shader/line.gs");
+		this->VAO = 0;
+		this->tMax = 50.0f;
+		this->color = glm::vec3(1.0f);
+		this->genVertexData();
+		
 	}
-	void setTMax(glm::vec3 rayEnd) {
+
+	void updateVAO() {
+		// reset VAO
+		this->VAO = 0;
+		this->genVertexData();
+	}
+	void setTMax(glm::vec3 rayEnd, bool updateVAO = true) {
 		this->tMax = glm::distance(this->origin, rayEnd);
+		//cout << this->tMax << endl;
+		if (updateVAO) 
+			this->updateVAO();
+		
+	}
+
+	void setTMax(float tMax,bool updateVAO = true) {
+		this->tMax = tMax;
+		
+		if (updateVAO)
+			this->updateVAO();
+		
+	}
+
+	void setColor(glm::vec3 color,bool updateVAO=true) {
+		this->color = color;
+
+		if (updateVAO)
+			this->updateVAO();
+	}
+	void genVertexData() {
+		// initialize (if necessary)
+		
+		if (this->VAO == 0)
+		{
+			this->vertexData.clear();
+			this->vertexData.push_back(this->origin.x);
+			this->vertexData.push_back(this->origin.y);
+			this->vertexData.push_back(this->origin.z);
+			glm::vec3 end = this->origin + this->dir * this->tMax;
+			
+			this->vertexData.push_back(end.x);
+			this->vertexData.push_back(end.y);
+			this->vertexData.push_back(end.z);
+			// color
+			this->vertexData.push_back(this->color.x);
+			this->vertexData.push_back(this->color.y);
+			this->vertexData.push_back(this->color.z);
+			glGenVertexArrays(1, &this->VAO);
+			glGenBuffers(1, &this->VBO);
+			// fill buffer
+			glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(float) * this->vertexData.size(), this->vertexData.data(), GL_STATIC_DRAW);
+			// link vertex attributes
+			glBindVertexArray(this->VAO);
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9* sizeof(float), (void*)0);
+			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(3 * sizeof(float)));
+			glEnableVertexAttribArray(2);
+			glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(6 * sizeof(float)));
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glBindVertexArray(0);
+		}
+	}
+	void draw(const glm::mat4& projection, const glm::mat4& view,float trans=1.0f) {
+		// render Cube
+		this->shader->use();
+		this->shader->setMat4("view", view);
+		this->shader->setMat4("projection", projection);
+		this->shader->setFloat("transparency", trans);
+		glBindVertexArray(this->VAO);
+		glDrawArrays(GL_POINTS, 0, 1);
+		glBindVertexArray(0);
+	}
+
+	static glm::vec3 reflectRay(glm::vec3 in, glm::vec3 normal) {
+		return in - 2.0f * normal * glm::dot(in, normal);
+	};
+
+	static float refractAngle(float inAngle, float inIndex, float outIndex) {
+		return glm::asin(inIndex * glm::sin(inAngle) /outIndex);
 	}
 };
 
--- header/ray/tracingRay.h
+++ header/ray/tracingRay.h
@@ -3,39 +3,128 @@
 
 #include "ray.h"
 #include "../object/object.h"
+#include "../light/pointLight.h"
 using namespace std;
-class TracingRay : Ray
+class TracingRay : public Ray
 {
 public:
-	
+
 	TracingRay* parent;
 	vector<TracingRay*> children;
 	vector<Object*>* objects;
-	float coeff;
-	int updated,level;
-	glm::vec3 color;
+	int updated, level;
+	bool isIntersect, isLightRay;
 	~TracingRay() {};
-	TracingRay(TracingRay* parent, int leftLevel,float coeff,glm::vec3 rayOrigin, glm::vec3 rayDir):Ray(rayOrigin,rayDir) {
+	TracingRay(TracingRay* parent, int leftLevel, glm::vec3 rayOrigin, glm::vec3 rayDir) :Ray(rayOrigin, rayDir) {
 		this->parent = parent;
-		this->coeff = coeff;
 		this->updated = 0;
 		this->level = leftLevel;
-	}
-	void updateValue(glm::vec3 childColor) {
-		this->updated += 1;
-		this->color += childColor;
-		if (this->updated == this->children.size())
-			this->parent->updateValue(this->color*this->coeff);
+		this->isIntersect = false;
 	}
 
-	void extract(glm::vec3 origin,glm::vec3 dir) {
+	void extract(glm::vec3 origin, glm::vec3 dir) {
 		if (this->level == 0)
 			return;
-		this->children.push_back(new TracingRay(this,this->level-1, 1,origin,dir));
+		this->children.push_back(new TracingRay(this, this->level - 1, origin, dir));
 	}
 
-	void trace() {
-	
+	bool canReach(Ray& ray, const vector<Object*>& objects, glm::vec3& terminal,Object* ignoreObj) {
+		float terminalDist = glm::length(terminal - ray.origin);
+		for (Object* obj : objects) {
+			if (obj == ignoreObj)continue;
+			float dis;
+			glm::vec3 tmpNormal;
+			// ignore object that it intersect
+		
+			if (obj->isIntersect(ray, dis, tmpNormal) && abs(dis) < terminalDist ) {
+				return false;
+			}
+		}
+		// have to set new color anyway, so don't update vao yet
+		ray.setTMax(terminal,false);
+
+		return true;
+	}
+
+	glm::vec3 trace(const vector<Object*>& objects, const vector<PointLight*>& lights,glm::vec3 viewPos,const glm::mat4& projection,const glm::mat4& view) {
+		this->color = glm::vec3(0);
+		glm::vec3 reflectColor(0.0f), reFracColor(0.0f);
+		float minDis = 1e3;
+		Object* intersected = nullptr;
+		glm::vec3 normal;
+		for (Object* obj : objects) {
+			float dis = 0;
+			glm::vec3 tmpNormal;
+			if (obj->isIntersect(*this, dis, tmpNormal) && dis < minDis) {
+				minDis = dis;
+				intersected = obj;
+				normal = tmpNormal;
+			}
+		}
+		if (intersected == nullptr) {
+			
+		}
+		else {
+			this->isIntersect = true;
+			glm::vec3 intersectedPoint = this->origin + this->dir * minDis;
+			// trace more rays		
+			if (this->level != 0) {
+				if (intersected->material.reflectCoeff != 0.0f) {
+					glm::vec3 reflectDir = Ray::reflectRay(this->dir, normal);
+					TracingRay* relfectRay = new TracingRay(this, this->level - 1, intersectedPoint, reflectDir);
+					reflectColor = relfectRay->trace(objects, lights, viewPos, projection, view);
+				}
+				if (intersected->material.refractCoeff != 0.0f) {
+					// air index is 1
+					float inAngle = glm::acos(abs(glm::dot(this->dir, normal)));
+					float outAngle = Ray::refractAngle(inAngle, 1,intersected->material.index);
+					//glm::vec3 refractDir = 
+					//TracingRay* refractRay = new TracingRay(this, this->level - 1, intersectedPoint, refractDir);
+					//reflectColor = refractRay->trace(objects, lights, viewPos, projection, view);
+				}
+			}
+
+			// trace light ray
+			for (PointLight* light : lights) {
+				Ray lightRay = Ray(intersectedPoint, light->position - intersectedPoint);
+				if (this->canReach(lightRay,objects,light->position,intersected)){
+					glm::vec3 colorFromLight = computePhong(*light,*intersected, normal,intersectedPoint, viewPos-intersectedPoint);
+					this->color += colorFromLight;
+					lightRay.setColor(colorFromLight);
+				}
+				else {
+					lightRay.setColor(glm::vec3(0.3f));
+				}
+				lightRay.draw(projection, view, 0.5f);
+				
+			}
+		}
+		this->setTMax(minDis);
+		this->draw(projection,view);
+		return this->color;
+	}
+
+	glm::vec3 computePhong(const PointLight& light,const Object& object,glm::vec3 normal,glm::vec3 fragPos,glm::vec3 viewDir) {
+
+		glm::vec3 lightDir = normalize(light.position - fragPos);
+		// diffuse shading
+		float diff = std::max(glm::dot(normal, lightDir), 0.0f);
+		// specular shading
+		glm::vec3 reflectDir = reflect(-lightDir, normal);
+		float spec = pow(std::max(dot(viewDir, reflectDir), 0.0f), object.material.shininess);
+		// attenuation
+		float distance = length(light.position - fragPos);
+		float attenuation = 1.0 / (light.pointLightProp.constant + light.pointLightProp.linear * distance +
+			light.pointLightProp.quadratic * (distance * distance));
+		// combine results
+		glm::vec3 ambient = light.prop.ambient * object.material.ambient;
+		glm::vec3 diffuse = light.prop.diffuse * diff * object.material.diffuse;
+		glm::vec3 specular = light.prop.specular * spec * object.material.specular;
+		ambient *= attenuation;
+		diffuse *= attenuation;
+		specular *= attenuation;
+		return (ambient + diffuse + specular);
+		
 	}
 };
 
